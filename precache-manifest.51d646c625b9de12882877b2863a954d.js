db9520ce0ee66f67c5cf203609d3911e76b89732
felsi13 <13felsi@gmail.com>
2019-07-26 14:18:14 +0300

    Updates

---
+++ precache-manifest.51d646c625b9de12882877b2863a954d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e1d1119f6bf1a6d8aa459a0418085b",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "803f6c3ed0a3d1648228",
+    "url": "/react_add-todo-form/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "bd0c9146313fb134b61e",
+    "url": "/react_add-todo-form/static/js/2.7c0a1338.chunk.js"
+  },
+  {
+    "revision": "803f6c3ed0a3d1648228",
+    "url": "/react_add-todo-form/static/js/main.b9d6206e.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
